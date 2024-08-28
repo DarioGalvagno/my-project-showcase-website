b61e7174ad025e59a31a282034af7ec4925832df
dario <darioggalvagno@outlook.com>
2024-08-28 13:18:30 +0200

    add description

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ import streamlit as st
 
 st.set_page_config(layout="wide")
 col1, col2 = st.columns(2)
+
 with col1:
     st.image("images/photo.png")
 with col2:
@@ -13,3 +14,11 @@ with col2:
     Thanks for stopping by, and I hope you enjoy exploring my work!
     """
     st.info(content)
+with st.container():
+    st.info("Below you can find some of the apps I have built in Python. Feel free to contact me!")
+## alternatively you can can use the following below
+# content2 = """
+# Below you can find some of the apps I have built in Python. Feel free to contact me!
+# """
+# st.info(content2)
+
