8f4ca98f247859a64a90d9f072de0e25232344e4
dario <darioggalvagno@outlook.com>
2024-08-29 20:41:31 +0200

    last update

--- send_email.py
+++ send_email.py
@@ -1,15 +1,16 @@
+import os
 import smtplib, ssl
-from credentials import email_username, email_password  # Importing the credentials
 
 def send_email(message):
     host = "smtp.gmail.com"
     port = 465
-    username = email_username  # Use the imported username
-    password = email_password  # Use the imported password
+    username = os.getenv("EMAIL_USERNAME")
+    password = os.getenv("EMAIL_PASSWORD")
 
-    receiver = "youremail@gmail.com"
+    receiver = "darioggalvagno@gmail.com"
     context = ssl.create_default_context()
 
     with smtplib.SMTP_SSL(host, port, context=context) as server:
         server.login(username, password)
         server.sendmail(username, receiver, message)
+
